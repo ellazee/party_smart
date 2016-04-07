47cf49076fdb71e2710ff8fbfcb4f3aa3a823154
Ella Erickson <ellazerickson@gmail.com>
2016-04-07 11:55:53 -0700

    data is persisting in drink tracker now

--- public/app/controllers.js
+++ public/app/controllers.js
@@ -19,7 +19,7 @@ angular.module("AppCtrls", ['PartyServices'])
 }])
 .controller('LoginCtrl', ['$scope', '$http', '$location', 'Auth', 'User', function($scope, $http, $location, Auth, User) {
 	$scope.hideOnLoggedIn = true;
-	
+
 	$scope.user = {
 		username: '',
 		email: '',
@@ -100,11 +100,11 @@ angular.module("AppCtrls", ['PartyServices'])
 	$scope.bacOverTime = localStorage.getItem('bacOverTime');
 }])
 .controller('TrackCtrl', ['$scope', 'User','$filter', function($scope, User, $filter) {
-	$scope.drinktracking = [];
-	$scope.beercounter = 0;
-	$scope.winecounter = 0;
-	$scope.cocktailcounter = 0;
+	$scope.beercounter = parseInt(localStorage.getItem('beercount')) || 0;
+	$scope.winecounter = parseInt(localStorage.getItem('winecount')) || 0;
+	$scope.cocktailcounter = parseInt(localStorage.getItem('cocktailcount')) || 0;
 	$scope.username = localStorage.getItem('username');
+
 	$scope.trackerOn = false;
 
 	// User = res.data.user;
@@ -116,14 +116,24 @@ angular.module("AppCtrls", ['PartyServices'])
 
 		console.log($scope.startMathTime);
 	}
+	if (($scope.beercounter + $scope.winecounter + $scope.cocktailcounter) > 0) {
+		console.log("Already beer-ed");
+		$scope.startTracker();
+	}
 	$scope.addBeer = function() {
 		$scope.beercounter++;
+		localStorage.setItem('beercount', $scope.beercounter);
+		// $scope.beercount = parseInt(localStorage.getItem('beercount'));
 	}
 	$scope.addWine = function() {
 		$scope.winecounter++;	
+		localStorage.setItem('winecount', $scope.winecounter);
+		// $scope.winecount = parseInt(localStorage.getItem('winecount'));
 	}
 	$scope.addCocktail = function() {
 		$scope.cocktailcounter++;	
+		localStorage.setItem('cocktailcount', $scope.cocktailcounter);
+		// $scope.cocktailcount = parseInt(localStorage.getItem('cocktailcount'));
 		// console.log($scope.startTime);
 
 	}
@@ -131,10 +141,17 @@ angular.module("AppCtrls", ['PartyServices'])
 		$scope.gender = localStorage.getItem('usergender');
 		$scope.weight = localStorage.getItem('userweight');
 		$scope.calcTime = Date.parse(new Date());
-		console.log($scope.weight);
+		// $scope.beercount = localStorage.getItem('beercount');
+		// $scope.winecount = localStorage.getItem('winecount');
+		// $scope.cocktailcount = localStorage.getItem('cocktailcount');
+
+		console.log("Beercount is" + $scope.beercount);
+		console.log("Winecount is" + $scope.winecount);
+		console.log("Cocktail count is" + $scope.cocktailcount);
 		$scope.weightInGrams = $scope.weight * 454;
 		console.log("User weight in grams is" + $scope.weightInGrams);
 		console.log("User gender is" + $scope.gender);
+		console.log($scope.beercount + $scope.winecount);
 		$scope.alcolholInGrams = ($scope.beercounter + $scope.winecounter + $scope.cocktailcounter) * 14;
 		console.log("Alcohol in grams is" + $scope.alcolholInGrams);
 		if($scope.gender == 'female') {
@@ -142,25 +159,17 @@ angular.module("AppCtrls", ['PartyServices'])
 			} else {
 			$scope.genderConstant =	$scope.weightInGrams * .68
 			};
-
-		console.log("gender constant is" + $scope.genderConstant);
-		console.log("Calc time is" + $scope.calcTime);
-		console.log("Start time is" + $scope.startMathTime);
-		console.log("Math of time is " + ($scope.calcTime - $scope.startMathTime));
-		console.log("In minutes: "+ ($scope.calcTime - $scope.startMathTime)/(60000));
-		console.log("Hours passed: "+ (($scope.calcTime - $scope.startMathTime)/(60000))/60);
+		// console.log("gender constant is" + $scope.genderConstant);
+		// console.log("Calc time is" + $scope.calcTime);
+		// console.log("Start time is" + $scope.startMathTime);
+		// console.log("Math of time is " + ($scope.calcTime - $scope.startMathTime));
+		// console.log("In minutes: "+ ($scope.calcTime - $scope.startMathTime)/(60000));
+		// console.log("Hours passed: "+ (($scope.calcTime - $scope.startMathTime)/(60000))/60);
 		$scope.hoursPassed = (($scope.calcTime - $scope.startMathTime)/(60000))/60;
-
-		// $scope.milliseconds = ($scope.startMathTime/(1000*60*60)) - ($scope.calcTime);
-		// $scope.minutesPassed = ($scope.startMathTime/(1000*60));
-		// console.log("Hours: " + $scope.hoursPassed)
-		// console.log("Minutes: " + $scope.minutesPassed)
-		// minutes = (int) ((milliseconds / (1000*60)) % 60);
-		// int hours   = (int) ((milliseconds / (1000*60*60)) % 24)
 		$scope.bac = ($scope.alcolholInGrams/$scope.genderConstant) * 100;
-		console.log("BAC is "+ $scope.bac);
+		// console.log("BAC is "+ $scope.bac);
 		$scope.bacTracking = $scope.bac - ($scope.hoursPassed * 0.015);
-		console.log($scope.bacTracking);
+		// console.log($scope.bacTracking);
 		localStorage.setItem('bacTracking', $scope.bacTracking);
 		$scope.showBAC = true;
 		}
@@ -170,27 +179,27 @@ angular.module("AppCtrls", ['PartyServices'])
 	}	
 	
 }])
-.controller('TimerCtrl', ['$scope', '$interval', '$filter', function($scope, $interval, $filter) {
-	$scope.time = $filter('date')(new Date(), 'HH:mm:ss');
-	$scope.newTime = $scope.time + $filter('date')('00:01:00');
-	$scope.startTimer = function() {
-		$scope.Timer = 60;
+.controller('TimerCtrl', ['$scope', '$filter', function($scope, $filter) {
+	// $scope.time = $filter('date')(new Date(), 'HH:mm:ss');
+	// $scope.newTime = $scope.time + $filter('date')('00:01:00');
+	// $scope.startTimer = function() {
+	// 	$scope.Timer = 60;
 
-		$scope.Message = "Timer started. ";
-		$scope.intervalId = $interval(function() {
-			$scope.Timer--
-		}, 1000)
-	}
-	$scope.StopTimer = function () {
+	// 	$scope.Message = "Timer started. ";
+	// 	$scope.intervalId = $interval(function() {
+	// 		$scope.Timer--
+	// 	}, 1000)
+	// }
+	// $scope.StopTimer = function () {
 
-        //Set the Timer stop message.
-        $scope.Message = "Timer stopped.";
+ //        //Set the Timer stop message.
+ //        $scope.Message = "Timer stopped.";
 
-        //Cancel the Timer.
-        if (angular.isDefined($scope.intervalId)) {
-            $interval.cancel($scope.intervalId);
-        }
-    };
+ //        //Cancel the Timer.
+ //        if (angular.isDefined($scope.intervalId)) {
+ //            $interval.cancel($scope.intervalId);
+ //        }
+ //    };
 
 }]);
 
